eec4a3c2fc8d65c7fe58ab9b81c970e78abe2162
Lauren Sheridan <laurensh@umich.edu>
2018-12-11 18:56:03 -0500

    updated csv and sql files

--- input/sql/flight_delays.sql
+++ input/sql/flight_delays.sql
@@ -1,6 +1,7 @@
 SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS airport, flight, airline, aircraft, temp_airport, temp_airline, iatacode, airline_iata, airportname, city, state, country, latitude, longitude;
+DROP TABLE IF EXISTS airport, flight, airline, aircraft, temp_airport, temp_flight, temp_airline, city, state;
 SET FOREIGN_KEY_CHECKS=1;
+
 
 CREATE TABLE IF NOT EXISTS temp_airport (
   airport_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
@@ -14,53 +15,17 @@ CREATE TABLE IF NOT EXISTS temp_airport (
   PRIMARY KEY (airport_id)
 )
 ENGINE=InnoDB
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;
+CHARACTER SET ascii
+COLLATE ascii_general_ci;
 
-LOAD DATA LOCAL INFILE './input/csv/airports.csv'
+LOAD DATA LOCAL INFILE './output/airlines/airports_trimmed.csv'
 INTO TABLE temp_airport
-  CHARACTER SET utf8mb4
+  CHARACTER SET ascii
   FIELDS TERMINATED BY ','
   ENCLOSED BY '"'
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (iata_code, airport_name, city, state, country, latitude, longitude);
-
-CREATE TABLE IF NOT EXISTS iatacode (
-  iata_code_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-  iata_code_name VARCHAR(100) NOT NULL,
-  PRIMARY KEY (iata_code_id)
-)
-ENGINE=InnoDB
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;
-
-LOAD DATA LOCAL INFILE './input/csv/airports.csv'
-INTO TABLE iatacode
-  CHARACTER SET utf8mb4
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (iata_code_name, @dummy, @dummy, @dummy, @dummy, @dummy, @dummy);
-
-CREATE TABLE IF NOT EXISTS airportname (
-  airport_name_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-  airportname VARCHAR(100) NOT NULL,
-  PRIMARY KEY (airport_name_id)
-)
-ENGINE=InnoDB
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;
-
-LOAD DATA LOCAL INFILE './input/csv/airports.csv'
-INTO TABLE airportname
-  CHARACTER SET utf8mb4
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (@dummy, @airportname, @dummy, @dummy, @dummy, @dummy, @dummy);
 
 CREATE TABLE IF NOT EXISTS city (
   city_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
@@ -68,89 +33,36 @@ CREATE TABLE IF NOT EXISTS city (
   PRIMARY KEY (city_id)
 )
 ENGINE=InnoDB
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;
+CHARACTER SET ascii
+COLLATE ascii_general_ci;
 
-LOAD DATA LOCAL INFILE './input/csv/airports.csv'
+LOAD DATA LOCAL INFILE './output/airlines/airports_trimmed.csv'
 INTO TABLE city
-  CHARACTER SET utf8mb4
+  CHARACTER SET ascii
   FIELDS TERMINATED BY ','
   ENCLOSED BY '"'
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (@dummy, @dummy, city_name, @dummy, @dummy, @dummy, @dummy);
 
-CREATE TABLE IF NOT EXISTS state (
-  state_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-  state_name VARCHAR(45) NOT NULL,
-  PRIMARY KEY (state_id)
-)
-ENGINE=InnoDB
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;
+-- CREATE TABLE IF NOT EXISTS state (
+--   state_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+--   state_name VARCHAR(45) NOT NULL,
+--   PRIMARY KEY (state_id)
+-- )
+-- ENGINE=InnoDB
+-- CHARACTER SET ascii
+-- COLLATE ascii_general_ci;
+--
+-- LOAD DATA LOCAL INFILE './output/airlines/airports_trimmed.csv'
+-- INTO TABLE state
+--   CHARACTER SET ascii
+--   FIELDS TERMINATED BY ','
+--   ENCLOSED BY '"'
+--   LINES TERMINATED BY '\n'
+--   IGNORE 1 LINES
+--   (@dummy, @dummy, @dummy, state_name, @dummy, @dummy, @dummy);
 
-LOAD DATA LOCAL INFILE './input/csv/airports.csv'
-INTO TABLE state
-  CHARACTER SET utf8mb4
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (@dummy, @dummy, @dummy, state_name, @dummy, @dummy, @dummy);
-
-CREATE TABLE IF NOT EXISTS country (
-  country_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-  country_name VARCHAR(45) NOT NULL,
-  PRIMARY KEY (country_id)
-)
-ENGINE=InnoDB
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;
-
-LOAD DATA LOCAL INFILE './input/csv/airports.csv'
-INTO TABLE country
-  CHARACTER SET utf8mb4
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (@dummy, @dummy, @dummy, @dummy, country_name, @dummy, @dummy);
-
-CREATE TABLE IF NOT EXISTS latitude (
-  latitude_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-  latitude_name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (latitude_id)
-)
-ENGINE=InnoDB
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;
-
-LOAD DATA LOCAL INFILE './input/csv/airports.csv'
-INTO TABLE latitude
-  CHARACTER SET utf8mb4
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (@dummy, @dummy, @dummy, @dummy, @dummy, latitude_name, @dummy);
-
-CREATE TABLE IF NOT EXISTS longitude (
-  longitude_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-  longitude_name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (longitude_id)
-)
-ENGINE=InnoDB
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_0900_ai_ci;
-
-LOAD DATA LOCAL INFILE './input/csv/airports.csv'
-INTO TABLE longitude
-  CHARACTER SET utf8mb4
-  FIELDS TERMINATED BY ','
-  ENCLOSED BY '"'
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES
-  (@dummy, @dummy, @dummy, @dummy, @dummy, @dummy, longitude_name);
 
 CREATE TABLE IF NOT EXISTS temp_airline (
   airline_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
@@ -159,35 +71,79 @@ CREATE TABLE IF NOT EXISTS temp_airline (
   PRIMARY KEY (airline_id)
 )
   ENGINE=InnoDB
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_0900_ai_ci;
+  CHARACTER SET ascii
+  COLLATE ascii_general_ci;
 
-  LOAD DATA LOCAL INFILE './input/csv/airlines.csv'
+  LOAD DATA LOCAL INFILE './output/airlines/airlines_trimmed.csv'
   INTO TABLE temp_airline
-    CHARACTER SET utf8mb4
+    CHARACTER SET ascii
     FIELDS TERMINATED BY ','
     ENCLOSED BY '"'
     LINES TERMINATED BY '\n'
     IGNORE 1 LINES
     (iata_code, airline_name);
 
-CREATE TABLE IF NOT EXISTS airline_iata (
-  airline_iata_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-  airline_iata_name CHAR(2) NOT NULL,
-  PRIMARY KEY (airline_iata_id)
+CREATE TABLE IF NOT EXISTS temp_flight (
+  flight_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+  year INTEGER NOT NULL,
+  month INTEGER NOT NULL,
+  day INTEGER NOT NULL,
+  day_of_week INTEGER NOT NULL,
+  airline CHAR(2) NOT NULL,
+  flight_number INTEGER NOT NULL,
+  tail_number VARCHAR(6) NOT NULL,
+  origin_airport CHAR(3) NOT NULL,
+  destination_airport CHAR(3) NOT NULL,
+  scheduled_departure INTEGER NOT NULL,
+  departure_time VARCHAR(50) NOT NULL,
+  departure_delay VARCHAR(50) NOT NULL,
+  taxi_out VARCHAR(50) NOT NULL,
+  wheels_off VARCHAR(50) NOT NULL,
+  scheduled_time VARCHAR(50) NOT NULL,
+  elapsed_time VARCHAR(50) NOT NULL,
+  air_time VARCHAR(50) NOT NULL,
+  distance INTEGER NOT NULL,
+  wheels_on VARCHAR(50) NOT NULL,
+  taxi_in VARCHAR(50) NOT NULL,
+  scheduled_arrival INTEGER NOT NULL,
+  arrival_time VARCHAR(50) NOT NULL,
+  arrival_delay VARCHAR(50) NOT NULL,
+  diverted INTEGER NOT NULL,
+  cancelled INTEGER NOT NULL,
+  PRIMARY KEY (flight_id)
 )
   ENGINE=InnoDB
-  CHARACTER SET utf8mb4
-    COLLATE utf8mb4_0900_ai_ci;
+  CHARACTER SET ascii
+  COLLATE ascii_general_ci;
 
-  LOAD DATA LOCAL INFILE './input/csv/airlines.csv'
-  INTO TABLE airline_iata
-    CHARACTER SET utf8mb4
+  LOAD DATA LOCAL INFILE './output/airlines/flights_trimmed.csv'
+  INTO TABLE temp_flight
+    CHARACTER SET ascii
     FIELDS TERMINATED BY ','
     ENCLOSED BY '"'
     LINES TERMINATED BY '\n'
     IGNORE 1 LINES
-    (airline_iata_name, @dummy);
+    (year, month, day, day_of_week, airline, flight_number, tail_number, origin_airport, destination_airport, scheduled_departure, departure_time, departure_delay, taxi_out, wheels_off, scheduled_time, elapsed_time, air_time, distance, wheels_on, taxi_in, scheduled_arrival, arrival_time, arrival_delay, diverted, cancelled);
+
+-- CREATE TABLE IF NOT EXISTS temp_airline_flight (
+--   airline_flight_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+--   flight_id INTEGER NOT NULL,
+--   airline_name VARCHAR(100) NOT NULL,
+--   PRIMARY KEY (airline_flight_id)
+-- )
+--   ENGINE=InnoDB
+--   CHARACTER SET ascii
+--   COLLATE ascii_general_ci;
+--
+--   LOAD DATA LOCAL INFILE './output/airlines/airports_trimmed.csv'
+--   INTO TABLE airport
+--   CHARACTER SET ascii
+--   FIELDS TERMINATED BY ','
+--   ENCLOSED BY '"'
+--   LINES TERMINATED BY '\n'
+--   IGNORE 1 LINES
+--   ();
+
 
 CREATE TABLE IF NOT EXISTS airport (
   airport_id INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
@@ -201,37 +157,30 @@ CREATE TABLE IF NOT EXISTS airport (
   PRIMARY KEY (airport_id)
 )
   ENGINE=InnoDB
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_0900_ai_ci;
+  CHARACTER SET ascii
+  COLLATE ascii_general_ci;
 
-  LOAD DATA LOCAL INFILE './input/csv/airports.csv'
+  LOAD DATA LOCAL INFILE './output/airlines/airports_trimmed.csv'
   INTO TABLE airport
-  CHARACTER SET utf8mb4
+  CHARACTER SET ascii
   FIELDS TERMINATED BY ','
   ENCLOSED BY '"'
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (iata_code, airport_name, city, state, country, latitude, longitude);
 
--- INSERT IGNORE INTO airport (
---   iata_code_id,
---   airport_name_id,
---   city_id,
---   state_id,
---   country_id,
---   latitude_id,
---   longitude_id,
--- )
-
-INSERT IGNORE INTO airline (
-airline_id,
-airline_name,
-airline_iata_code_id
+INSERT IGNORE INTO airport (
+  iata_code,
+  airport_name,
+  city,
+  state,
+  country,
+  latitude,
+  longitude
 )
 
-
-SELECT ta.airline_id, ta.airline_name, ta.airline_iata_id
-  FROM temp_airline ta
-       LEFT JOIN airline_iata ai
-              ON TRIM(ta.airline_name) = TRIM(ai.airline_name)
-ORDER BY ta.airline_name;
+SELECT ta.airport_name, ta.city
+  FROM temp_airport ta
+       LEFT JOIN city cit
+              ON TRIM(ta.city) = TRIM(cit.city_name)
+ORDER BY ta.airport_name;
